ab2481625382e0671ac575c3c463a67a75fefa0a
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-27 12:53:13 +0000

    Fixed bug in query used for installation in multisite environments.
    git-svn-id: https://plugins.svn.wordpress.org/cp-contact-form-with-paypal/trunk@1076626 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cp_contactformpp.php
+++ cp_contactformpp.php
@@ -133,7 +133,7 @@ function cp_contactformpp_install($networkwide)  {
 		if ($networkwide) {
 	                $old_blog = $wpdb->blogid;
 			// Get all blog ids
-			$blogids = $wpdb->get_col($wpdb->prepare("SELECT blog_id FROM $wpdb->blogs"));
+			$blogids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
 			foreach ($blogids as $blog_id) {
 				switch_to_blog($blog_id);
 				_cp_contactformpp_install();
